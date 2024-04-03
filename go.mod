294f4692c6227c784f7c5ccc4b50002db900dddc
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-04-03 23:33:38 +0300

    handlers

--- go.mod
+++ go.mod
@@ -4,9 +4,10 @@ go 1.22
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	ssov1 "sso/protobuf"
 )
 
@@ -15,5 +17,17 @@ func Register(gRPC *grpc.Server) {
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
 
+	return &ssov1.LoginResponse{Token: ""}, nil
+}
+
+func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
+	panic("implement this")
+}
+
+func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
+	panic("implement this")
 }

40ee6457079255d3b2df6ececa1627106898a287
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-04-04 00:20:46 +0300

    auth interface

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,1 +1,55 @@
 package auth
+
+import (
+	"context"
+	"log/slog"
+	"sso/internal/domain/models"
+	"time"
+)
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTLL    time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+func New(log *slog.Logger, userSaver UserSaver, userProvider UserProvider, appProvider AppProvider, tokenTLL time.Duration) *Auth {
+	return &Auth{
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		log:         log,
+		appProvider: appProvider,
+		tokenTLL:    tokenTLL,
+	}
+}
+
+func (a *Auth) Login(ctx context.Context, email string, password string, appID int) (string, error) {
+	panic("not implemented")
+}
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (int64, error) {
+	panic("not implemented")
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	panic("not implemented")
+}

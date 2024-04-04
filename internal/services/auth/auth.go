fc26c790ba5ef1eacc845694998119dab0b8a20a
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-04-04 22:41:59 +0300

    migration

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -39,6 +39,8 @@ type AppProvider interface {
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidAppID       = errors.New("invalid app id")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 func New(log *slog.Logger, userSaver UserSaver, userProvider UserProvider, appProvider AppProvider, tokenTLL time.Duration) *Auth {
@@ -115,6 +117,12 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (
 
 	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists", err.Error())
+
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
+
 		log.Error("failed to save user", err.Error())
 
 		return 0, fmt.Errorf("%s: %w", op, err)
@@ -138,6 +146,11 @@ func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
 	isAdmin, err := a.usrProvider.IsAdmin(ctx, userID)
 
 	if err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			log.Warn("user not found", err.Error())
+
+			return false, fmt.Errorf("%s: %w", op, ErrInvalidAppID)
+		}
 		return false, fmt.Errorf("%s: %w", op, err)
 	}
 
